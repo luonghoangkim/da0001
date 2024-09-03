8d3504abe02c74a490fdf45b5887deec53002416
Hoang Kim Luong <hoangkimluong192@gmail.com>
2024-09-03 17:20:19 +0700

    translate

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,34 +1,19 @@
-// import { Button } from "antd";
-// import {useTranslations} from 'next-intl';
-// import {Link} from '@/i18n/routing';
-
-
-// export default function Home() {
-//   const t = useTranslations('Login');
-//   return (
-//     <div>
-//     <h1>{t('login')}</h1>
-//     <Link href="/about">{t('username')}</Link>
-//   </div>
-//   );
-// }
-
-import {Link} from '@/i18n/routing';
+import { Link } from '@/i18n/routing';
+import { useTranslations } from 'next-intl';
 
 const Home = () => {
+  const t = useTranslations('Home');
+
   return (
-      <div style={{ maxWidth: '400px', margin: '0 auto', padding: '20px' }}>
-        <h1>Welcome to the Home Page</h1>
-        <p>
-          <Link href="/signup">
-            Go to Sign Up
-          </Link>
-        </p>
-      </div>
+    <div style={{ maxWidth: '400px', margin: '0 auto', padding: '20px' }}>
+      <h1>{t('welcomeMessage')}</h1>  
+      <p>
+        <Link href="/signup">
+          {t('goToSignUp')} 
+        </Link>
+      </p>
+    </div>
   );
 };
 
 export default Home;
-
-
-
