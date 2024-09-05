9c4fdb2224a7fce141481efa47a03b258a68e2b9
Hoang Kim Luong <hoangkimluong192@gmail.com>
2024-09-05 18:18:41 +0700

    config toats

--- src/app/[locale]/login/login-form.tsx
+++ src/app/[locale]/login/login-form.tsx
@@ -26,36 +26,43 @@ const LoginForm = () => {
 
     if (!isValidEmail(email)) {
       setError(t('invalidEmail'));
-      toast.error(t('invalidEmail')); // Hiển thị thông báo lỗi
+      toast.error(t('invalidEmail'));  
       return;
     }
 
     if (!password) {
       setError(t('invalidPassword'));
-      toast.error(t('invalidPassword')); // Hiển thị thông báo lỗi
+      toast.error(t('invalidPassword'));  
       return;
     }
 
     try {
       const res = await loginService(email, password);
 
-      if (res.status === 400) {
+      if (res.status === 404) {
         setError(t('emailExists'));
-        toast.error(t('emailExists')); // Hiển thị thông báo lỗi
-      } else if (res.status === 200) {
+        toast.error(t('emailExists'));  
+      } else if (res.status === 401) {
+        setError(t('wrongPassword'));
+        toast.error(t('wrongPassword')); 
+      }else if (res.status === 200) {
         setError('');
-        toast.success(t('loginSuccess')); // Hiển thị thông báo thành công
+        toast.success(t('loginSuccess')); 
         router.push('/dashboard');
+      }else{
+        setError(t('loginErr'));
+        toast.error(t('loginErr')); 
       }
     } catch (error) {
       setError(t('errorOccurred'));
-      toast.error(t('errorOccurred')); // Hiển thị thông báo lỗi
+      toast.error(t('errorOccurred')); 
       console.log(error);
     }
   };
 
   return (
     <div style={{ maxWidth: '400px', margin: '0 auto', padding: '50px 20px', textAlign: 'center' }}>
+      <ToastContainer />
       <Title level={3} style={{ color: '#37B29E', marginBottom: '40px' }}>MyFinanceManager.com</Title>
       <Title level={4}>{t('login')}</Title>
       <Form layout="vertical" onFinish={onFinish}>
@@ -86,7 +93,6 @@ const LoginForm = () => {
       <Text style={{ marginTop: '20px', display: 'block' }}>
         {t('notHaveAccount')} <Link href="/register">{t('signUp')}</Link>
       </Text>
-      {error && <Text type="danger" style={{ marginTop: '10px' }}>{error}</Text>}
     </div>
   );
 };
