9139efcd3d82e4529d22c713b38583296e1eb23f
Hoang Kim Luong <hoangkimluong192@gmail.com>
2024-09-12 17:47:56 +0700

    trans

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,6 +1,12 @@
+import moment from 'moment';
+
 export function formatCurrency(amount: number): string {
     return new Intl.NumberFormat('en-US', {
         minimumFractionDigits: 0,
         maximumFractionDigits: 0,
     }).format(amount);
+}
+
+export function formatDate(date: string): string {
+    return moment(date).format('HH:mm DD/MM/YYYY');
 }
