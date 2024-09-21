f5faf37af36d5cf5a8ff536cb1c277e24ea3eaab
Hoang Kim Luong <hoangkimluong192@gmail.com>
2024-09-21 20:45:27 +0700

    login

--- src/models/auth-modal/user.modal.ts
+++ src/models/auth-modal/user.modal.ts
@@ -1,39 +1,4 @@
-import mongoose from "mongoose";
-
-// Định nghĩa mô hình (model) MongoDB
-const { Schema } = mongoose;
-
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  phone_number: {
-    type: String,
-    trim: true,
-    default: null,
-  },
-  address: {
-    type: String,
-    default: null,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female"],
-    default: null,
-  },
-});
-
-const User = mongoose.models.User || mongoose.model("User", userSchema);
-
-export default User;
+export interface LoginPayload {
+  account_name: string;
+  password: string;
+}
--- src/service/login/login-service.ts
+++ src/service/login/login-service.ts
@@ -1,12 +1,12 @@
+import { LoginPayload } from '@/models/auth-modal/user.modal';
 import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const loginService = async (email: string, password: string) => {
+export const loginService = async (payload: LoginPayload) => {
   try {
-    const res = await axios.post(`${API_URL}/api/auth/login`, {
-      email,
-      password,
+    const res = await axios.post(`${API_URL}/api/v2/auths/login`, {
+      payload,
     }, {
       headers: {
         'Content-Type': 'application/json',
