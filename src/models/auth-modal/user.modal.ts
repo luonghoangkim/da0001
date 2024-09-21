cefd27890e9a4678f9c2a608768fc073cd097a86
Hoang Kim Luong <hoangkimluong192@gmail.com>
2024-09-21 21:56:05 +0700

    register

--- src/models/auth-modal/user.modal.ts
+++ src/models/auth-modal/user.modal.ts
@@ -2,3 +2,14 @@ export interface LoginPayload {
   account_name: string;
   password: string;
 }
+
+export interface RegisterPayload {
+  account_name: string;
+  email: string;
+  password: string;
+}
+
+export interface VerifyPayload {
+  email: string;
+  codeId: number;
+}
---
+++ src/service/register/register-service.ts
@@ -0,0 +1,43 @@
+import { RegisterPayload, VerifyPayload } from '@/models/auth-modal/user.modal';
+import axios from 'axios';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const register = async (payload: RegisterPayload) => {
+  try {
+    const res = await axios.post(`${API_URL}/api/v2/auths/register`, {
+      payload,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return res;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
+
+const verify = async (payload: VerifyPayload) => {
+  try {
+    const res = await axios.post(`${API_URL}/api/v2/auths/verify`, {
+      payload,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return res;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
+
+export const REGISTER_SERVICE = {
+  register,
+  verify
+}
